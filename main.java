e87fd68ec7aaf577a8ac53eae92176577a6109fc
Naman <chauhannaman242@gmail.com>
2023-12-13 16:07:28 +0530

    I dont know what this is

---
+++ main.java
@@ -0,0 +1,19 @@
+
+public class main {
+
+    String model;
+    String color;
+    int year;
+
+public static void main(String[] args) {
+    
+}
+
+public main(String model, String color, int year){
+
+this.model = model;
+this.color = color;
+this.year = year;
+
+}
+}
